46f3e212fd6508eb4dc528a157e50e9d54025d34
MatheusLPolidoro <Mattpolidoro4@gmail.com>
2021-11-05 20:44:31 -0300

    Aula UML

---
+++ Principal.java
@@ -0,0 +1,11 @@
+package pacote;
+
+public class Principal {
+	public static void main(String[] args) {
+	
+		Pessoa obj = new Pessoa();
+		obj.recebeDados();
+		obj.calculaIdade();
+		obj.mostrarDados(); 
+	}
+}
